4ccbab6ccd0a871f7c9cd57c4753ad9e19e3cd3f
alireza1465 <alirezayazdi1465@gmail.com>
2025-05-22 20:21:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 import telebot
+
+# 🔑 توکن رباتت
+TOKEN = "7635073258:AAHEd2yddm0ihR-l8Z_0P8UetkerdC77-gs"
+
+bot = telebot.TeleBot(TOKEN)
+
+# ❗ حل خطای Webhook
+bot.remove_webhook()
+
+# ✅ پاسخ به دستور /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "سلام! ربات سیگنال طلا فعال شد ✅")
+
+# ✅ فعال کردن Polling
+bot.polling()
