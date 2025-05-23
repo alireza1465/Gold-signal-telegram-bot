e482bab0287f60c2031050c87e5cb73af17114cc
alireza1465 <alirezayazdi1465@gmail.com>
2025-05-23 21:51:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام علیرضا! ربات سیگنال طلا آماده است 🚀")
 
 if __name__ == '__main__':
-    import os
-    TOKEN = os.getenv("TELEGRAM_TOKEN")
+    TOKEN = "7683451551:AAHMdA-g4bmU7wEPTWil6anSui9YVAhWbPM"
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
