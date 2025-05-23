fc227ecdad1878bb1de2cb425770a5f1664da826
alireza1465 <alirezayazdi1465@gmail.com>
2025-05-24 00:51:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from analyzer import get_gold_price, analyze_price
+import os
 
-TOKEN = os.environ.get("TOKEN")
+# دریافت توکن از متغیر محیطی
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام علیرضا! ربات سیگنال طلا آماده است 🚀")
 
-if __name__ == '__main__':
+async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    price = get_gold_price()
+    signal = analyze_price(price)
+    await update.message.reply_text(signal)
+
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("signal", signal))
     app.run_polling()
