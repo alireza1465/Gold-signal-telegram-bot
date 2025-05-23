5fcb40be62c1c17881c8dfd5a736ac1b87b2a919
alireza1465 <alirezayazdi1465@gmail.com>
2025-05-23 22:44:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from analyzer import get_gold_price, analyze_price
+import os
 
-# ⛔ توکن خودتو مستقیم اینجا وارد کن ⛔
 TOKEN = "7683451551:AAHMdA-g4bmU7wEPTWil6anSui9YVAhWbPM"
+WEBHOOK_URL = "https://gold-signal-bot.up.railway.app"  # لینک Railway رو بذار اینجا
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام علیرضا! ربات سیگنال طلا آماده است 🚀")
@@ -13,8 +14,12 @@ async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
     signal = analyze_price(price)
     await update.message.reply_text(signal)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("signal", signal))
-    app.run_polling()
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        webhook_url=f"{WEBHOOK_URL}/{TOKEN}"
+    )
