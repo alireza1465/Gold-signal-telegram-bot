281487ccbd5d1cde401ca91aa1506033439ff913
alireza1465 <alirezayazdi1465@gmail.com>
2025-05-24 11:47:45 +0200

    Update auto_sender.py

--- auto_sender.py
+++ auto_sender.py
@@ -1,17 +1,20 @@
 import os
 import time
 import requests
+from analyzer import get_gold_price, analyze_price
 
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 def send_signal():
+    price = get_gold_price()
+    signal = analyze_price(price)
+    message = f"{signal}\nقیمت فعلی: {price}"
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    message = "سیگنال تستی ارسال شد (ربات روشن است)"
     data = {"chat_id": CHAT_ID, "text": message}
     requests.post(url, data=data)
 
 if __name__ == "__main__":
     while True:
         send_signal()
-        time.sleep(60)
+        time.sleep(60)  # هر ۶۰ ثانیه یک‌بار
